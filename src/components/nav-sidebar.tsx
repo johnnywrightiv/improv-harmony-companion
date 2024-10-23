e8f73073a03fac577eaddbbcab84f5ab8bc97467
Johnny Wright <johnnywrightiv@gmail.com>
2024-10-22 23:32:44 -0500

    remove hidden class from nav-sidebar

--- src/components/nav-sidebar.tsx
+++ src/components/nav-sidebar.tsx
@@ -39,7 +39,7 @@ export default function NavSidebar() {
         ${isVisible ? 'lg:w-64' : 'lg:w-20'}`}
 			aria-label="Main Navigation"
 		>
-			<div className="relative hidden">
+			<div className="relative">
 				<Button
 					variant="ghost"
 					onClick={() => dispatch(toggleSidebar())}
